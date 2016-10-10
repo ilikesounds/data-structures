56a86e947f2686cd6f8b33fd76fe748a95fc21ef
ilikesounds <email_id>
2016-10-09 17:47:32 -0700

    init function for hash class created

--- src/hash_table.py
+++ src/hash_table.py
@@ -28,3 +28,26 @@ def simple_hash(key, size):
     for i in key:
         hash_value += i % size
     return hash_value
+
+
+class HashTable(object):
+
+    """
+    This class is a hash table. It contains 2 public(get, set) and 2 private
+    functions (__init__, _hash)to create a hash table.
+    """
+
+    def __init__(self, hash_func='simple_hash'):
+        self.size = 1024
+        self._buckets = [[]] * self.size
+        self.hash_functions = {
+            'simple_hash': simple_hash,
+            'bp_hash': bp_hash
+        }
+
+        if hash_func in self.hash_functions:
+            self.hash_func = self.hash_functions[hash_func]
+        else:
+            raise TypeError(
+                'You must select simple_hash or bp_hash as a hash function'
+                )
