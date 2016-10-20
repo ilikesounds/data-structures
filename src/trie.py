b3bfe2569b1d180ef17793f94743f34f53f3923c
Derek Hewitt <derekmhewitt@gmail.com>
2016-10-20 13:08:29 -0700

    more traversal work

--- src/trie.py
+++ src/trie.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """The following code implements a trie tree in python."""
 
+END = '$'
+
 
 class Trie(object):
     """Implement Trie tree class with supporting methods."""
@@ -14,35 +16,42 @@ class Trie(object):
         tokens = self.tokens
         if not isinstance(token, str):
             raise TypeError("Input must be a string.")
-        if '$' in token:
+        if END in token:
             raise ValueError("Input must not contain $ character.")
         for char in token:
             tokens = tokens.setdefault(char, {})
-        tokens['$'] = True
+        tokens[END] = True
 
     def contains(self, token):
         """Returns true if token is in trie, false if not."""
         if not isinstance(token, str):
             raise TypeError("Input must be a string.")
-        if '$' in token:
+        if END in token:
             raise ValueError("Input must not contain $ character.")
         tokens = self.tokens
         for char in token:
             if char not in tokens:
                 return False
             tokens = tokens[char]
-        return tokens.get('$', False)
+        return tokens.get(END, False)
 
     def traversal(self, start):
         """Performs a depth-first traversal of our trie beginning at the
         string we specify as start"""
         if not isinstance(start, str):
             raise TypeError("Input must be a string.")
-        if '$' in start:
+        if END in start:
             raise ValueError("Input must not contain $ character.")
         tokens = self.tokens
         for char in start:
-            tokens = tokens[char]
-        # step through the trie from here
-        # yeild each word as you come to it
-        # the type of traversal isn't important, just make one
+            if char in tokens:
+                tokens = tokens[char]
+        stepping_list = []
+        stepping_list.append((tokens, ""))
+        while stepping_list:
+            next_chars, word = stepping_list.pop()
+            if END in tokens:
+                yield start + word
+            for char in next_chars:
+                if char is not END:
+                    stepping_list.append((next_chars[char], start + char))
