03922101c6d0d3ddc3aa89e280b92002b01e3cf5
ilikesounds <jeffrey.n.torres@gmail.com>
2016-11-04 13:32:38 -0700

    fixed trie

--- src/trie.py
+++ src/trie.py
@@ -1,57 +1,66 @@
 # -*- coding: utf-8 -*-
-"""The following code implements a trie tree in python."""
+"""
+The following code implements a trie tree in python.
+
+Thanks to David Banks for help with the insert and contains methods:
+https://github.com/crashtack/data-structures/tree/trie
+"""
 
 END = '$'
 
+class Node(object):
+    """Implements nodes for our Trie tree."""
+
+    def __init__(self, value=None):
+        """Creates an instance of our trie node class."""
+        self.value = value
+        self.next = []
+
+
+    def __eq__(self, other):
+        """Method to check and compare if a letter is next"""
+        return self.value == other
+
+    def _insert(self, word):
+        """Insert a letter into letter list if there's a next value"""
+        try:
+            letter = Node(word[0])
+        except IndexError:
+            if END not in self.next:
+                self.next.append(Node(END))
+            return
+        if letter not in self.next:
+            self.next.append(letter)
+        index = self.next.index(letter)
+        self.next[index]._insert(word[1:])
+
+    def _contains(self, word):
+        """Check if next letter. Pass remaining word fragment to next node."""
+        try:
+            letter = word[0]
+        except IndexError:
+            if END in self.next:
+                return True
+            else:
+                return False
+        if END in self.next:
+            index = self.next.index(letter)
+            return self.next[index]._contains(word[1:])
+        else:
+            return False
+
 
 class Trie(object):
-    """Implement Trie tree class with supporting methods."""
+    """Trie tree class with supporting methods."""
 
     def __init__(self):
         """Initializes an empty Trie tree."""
-        self.tokens = {}
+        self.root = Node()
 
-    def insert(self, token):
-        """Inserts a new token into our trie tree."""
-        tokens = self.tokens
-        if not isinstance(token, str):
-            raise TypeError("Input must be a string.")
-        if END in token:
-            raise ValueError("Input must not contain $ character.")
-        for char in token:
-            tokens = tokens.setdefault(char, {})
-        tokens[END] = True
+    def insert(self, value):
+        """Inserts a new value into our trie tree."""
+        self.root._insert(value)
 
-    def contains(self, token):
-        """Returns true if token is in trie, false if not."""
-        if not isinstance(token, str):
-            raise TypeError("Input must be a string.")
-        if END in token:
-            raise ValueError("Input must not contain $ character.")
-        tokens = self.tokens
-        for char in token:
-            if char not in tokens:
-                return False
-            tokens = tokens[char]
-        return tokens.get(END, False)
-
-    def traversal(self, start):
-        """Performs a depth-first traversal of our trie beginning at the
-        string we specify as start"""
-        if not isinstance(start, str):
-            raise TypeError("Input must be a string.")
-        if END in start:
-            raise ValueError("Input must not contain $ character.")
-        tokens = self.tokens
-        for char in start:
-            if char in tokens:
-                tokens = tokens[char]
-        stepping_list = []
-        stepping_list.append((tokens, ""))
-        while stepping_list:
-            next_chars, word = stepping_list.pop()
-            if END in tokens:
-                yield start + word
-            for char in next_chars:
-                if char is not END:
-                    stepping_list.append((next_chars[char], start + char))
+    def contains(self, value):
+        """Returns true if value is in trie, false if not."""
+        return self.root._contains(value)
